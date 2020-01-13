7a9e08203d039a965db8082d9170e707a9d3ccd7
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-13 12:58:02 +0300

    Remove uVote and dVote properties from the Quote class constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showMore: boolean
-    constructor(public uVote: number, public dVote: number, public quotation:string, public author:string, public submitter:string, public submitTime: Date){
+    constructor(public quotation:string, public author:string, public submitter:string, public submitTime: Date){
         this.showMore = true;
     }
 }
